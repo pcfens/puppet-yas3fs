db1a6801fbb91291e16797663fdb0b75525e3036
Jethro Carr <jethro.carr@jethrocarr.com>
2015-06-20 19:22:09 +1200

    Fixing approach to defaulting to upstart, can't redefine variables with Puppet DSL

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,16 @@
 # yas3fs defaults
 class yas3fs::params {
   $install_pip_package = true
-  $install_init        = $::initsystem
 
-  # Compatibility mode for existing users, if missing the jethrocarr-initfact
-  # module, we fall back to using the upstart init script/configuration.
-  if (!$install_init) {
+
+  # Use the jethrocarr-initfact module to determine which init system is in
+  # use, fall back to using upstart if missing to ensure compatibility with
+  # exiting puppet-yas3fs users.
+
+  if ($::initsystem) {
+    $install_init = $::initsystem
+  } else {
     $install_init = 'upstart'
   }
+
 }
