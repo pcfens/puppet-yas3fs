08bcbbdecc413eb55495113944ad12565057ffb8
Jethro Carr <jethro.carr@jethrocarr.com>
2015-06-20 18:18:26 +1200

    Avoid surprises for existing users, if jethrocarr-initfact module not installed, default to using upstart still

--- manifests/params.pp
+++ manifests/params.pp
@@ -2,4 +2,10 @@
 class yas3fs::params {
   $install_pip_package = true
   $install_init        = $::initsystem
+
+  # Compatibility mode for existing users, if missing the jethrocarr-initfact
+  # module, we fall back to using the upstart init script/configuration.
+  if (!$install_init) {
+    $install_init = 'upstart'
+  }
 }
