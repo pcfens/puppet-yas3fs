408845f935a6e9ea0ec1a3b528ea8b0fe7a48b66
Ron Aughenbaugh <raughenb@umd.edu>
2022-07-08 12:09:21 -0400

    Rubocop fixes

--- spec/classes/yas3fs_spec.rb
+++ spec/classes/yas3fs_spec.rb
@@ -1,38 +1,39 @@
 require 'spec_helper'
-describe 'yas3fs', :type => :class do
-
-  let :facts do {
-    :osfamily => 'Debian',
-    :initsystem => 'upstart'
-  } end
-
+describe 'yas3fs', type: :class do
+  let :facts do
+    {
+      osfamily: 'Debian',
+    initsystem: 'upstart'
+    }
+  end
 
   context 'defaults' do
     it { is_expected.to contain_package('fuse') }
     it { is_expected.to contain_package('python-pip') }
 
-    it { is_expected.to contain_augeas('fuse.conf:user_allow_other').with(
+    it {
+      is_expected.to contain_augeas('fuse.conf:user_allow_other').with(
       'context' => '/files/etc/fuse.conf',
       'changes' => [
         'set user_allow_other ""',
-      ]
-    ) }
+      ],
+    )
+    }
 
     context 'with install_pip_package set to false' do
       let :params do
         {
-          :install_pip_package => false,
+          install_pip_package: false,
         }
       end
 
-      it { is_expected.not_to contain_package('python-pip')}
-
+      it { is_expected.not_to contain_package('python-pip') }
     end
 
     context 'with mounts in the initial resource creation' do
       let :params do
         {
-          :mounts => {
+          mounts: {
             'test-mount' => {
               's3_url'     => 's3://example-bucket/',
               'local_path' => '/media/s3',
@@ -41,18 +42,19 @@ describe 'yas3fs', :type => :class do
         }
       end
 
-      it { is_expected.to contain_file('yas3fs-test-mount').with(
+      it {
+        is_expected.to contain_file('yas3fs-test-mount').with(
         'ensure' => 'present',
         'path'   => '/etc/init/s3fs-test-mount.conf',
-      ) }
+      )
+      }
 
-      it { is_expected.to contain_service('s3fs-test-mount').with(
+      it {
+        is_expected.to contain_service('s3fs-test-mount').with(
         'ensure' => 'running',
         'enable' => true,
-      ) }
-
-
+      )
+      }
     end
-
   end
 end
--- spec/defines/mount_spec.rb
+++ spec/defines/mount_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-describe 'yas3fs::mount', :type => :define do
-
+describe 'yas3fs::mount', type: :define do
   let :pre_condition do
     'include ::yas3fs'
   end
@@ -12,78 +11,104 @@ describe 'yas3fs::mount', :type => :define do
 
   describe 'on all systems' do
     context 'with upstart' do
-      let :params do {
-        :s3_url     => 's3://test-bucket',
-        :local_path => '/media/test-mount',
-      } end
+      let :params do
+        {
+          s3_url: 's3://test-bucket',
+        local_path: '/media/test-mount',
+        }
+      end
 
-      let :facts do {
-        :initsystem => 'upstart',
-        :osfamily => 'Debian'
-      } end
+      let :facts do
+        {
+          initsystem: 'upstart',
+        osfamily: 'Debian'
+        }
+      end
 
-      it { is_expected.to contain_file('yas3fs-test-mount').with(
+      it {
+        is_expected.to contain_file('yas3fs-test-mount').with(
         'ensure' => 'present',
         'path'   => '/etc/init/s3fs-test-mount.conf',
         'notify' => 'Service[s3fs-test-mount]',
-      ) }
+      )
+      }
 
-      it { is_expected.to contain_service('s3fs-test-mount').with(
+      it {
+        is_expected.to contain_service('s3fs-test-mount').with(
         'ensure' => 'running',
         'enable' => true,
-      ) }
+      )
+      }
     end
 
     context 'with systemd' do
-      let :facts do {
-        :initsystem => 'systemd',
-        :osfamily => 'Debian'
-      } end
+      let :facts do
+        {
+          initsystem: 'systemd',
+        osfamily: 'Debian'
+        }
+      end
 
-      let :params do {
-        :s3_url     => 's3://test-bucket',
-        :local_path => '/media/test-mount',
-      } end
+      let :params do
+        {
+          s3_url: 's3://test-bucket',
+        local_path: '/media/test-mount',
+        }
+      end
 
-      it { is_expected.to contain_exec('yas3fs_reload_systemd-test-mount').with(
+      it {
+        is_expected.to contain_exec('yas3fs_reload_systemd-test-mount').with(
         'command'   => 'systemctl daemon-reload',
         'subscribe' => 'File[yas3fs-test-mount]',
         'before'    => 'Service[s3fs-test-mount]',
-      ) }
+      )
+      }
 
-      it { is_expected.to contain_file('yas3fs-test-mount').with(
+      it {
+        is_expected.to contain_file('yas3fs-test-mount').with(
         'ensure' => 'present',
         'path'   => '/etc/systemd/system/s3fs-test-mount.service',
         'notify' => 'Service[s3fs-test-mount]',
-      ) }
+      )
+      }
 
-      it { is_expected.to contain_service('s3fs-test-mount').with(
+      it {
+        is_expected.to contain_service('s3fs-test-mount').with(
         'ensure' => 'running',
         'enable' => true,
-      ) }
+      )
+      }
     end
 
     context 'with sysvinit' do
-      let :facts do {
-        :initsystem => 'sysvinit',
-        :osfamily => 'Debian'
-      } end
+      let :facts do
+        {
+          initsystem: 'sysvinit',
+        osfamily: 'Debian'
+        }
+      end
 
-      let :params do {
-        :s3_url     => 's3://test-bucket',
-        :local_path => '/media/test-mount',
-      } end
+      let :params do
+        {
+          s3_url: 's3://test-bucket',
+        local_path: '/media/test-mount',
+        }
+      end
 
-      it { is_expected.to contain_file('yas3fs-test-mount').with(
+      it {
+        is_expected.to contain_file('yas3fs-test-mount').with(
         'ensure' => 'present',
         'path'   => '/etc/init.d/s3fs-test-mount',
         'notify' => 'Service[s3fs-test-mount]',
-      ) }
+      )
+      }
 
-      it { is_expected.to contain_service('s3fs-test-mount').with(
+      it {
+        is_expected.to contain_service('s3fs-test-mount').with(
         'ensure' => 'running',
         'enable' => true,
-      ) }
+      )
+      }
     end
   end
 end
