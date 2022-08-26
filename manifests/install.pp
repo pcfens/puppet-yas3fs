bff20c6929f6afb226cea4defb65d0f05948ce1b
Ron Aughenbaugh <ronaugh+github@umd.edu>
2022-08-26 15:02:18 -0400

    pyvenv needs some base system pip packages

--- manifests/install.pp
+++ manifests/install.pp
@@ -33,7 +33,7 @@ class yas3fs::install (
     python::pyvenv { 'yas3fs virtual environment' :
       ensure     => present,
       version    => $_python_version,
-      systempkgs => false,
+      systempkgs => true,
       venv_dir   => $venv_path,
     }
   }
